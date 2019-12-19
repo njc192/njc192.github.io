7941888ffb41548ba5aacd0fdb966d2df258da87
njc192 <32405757+njc192@users.noreply.github.com>
2019-12-19 14:43:57 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+// create needed constants
+const rememberDiv = document.querySelector('.remember');
+const forgetDiv = document.querySelector('.forget');
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#entername');
+const submitBtn = document.querySelector('#submitname');
+const forgetBtn = document.querySelector('#forgetname');
+
+const h1 = document.querySelector('h1');
+const personalGreeting = document.querySelector('.personal-greeting');
+
+form.addEventListener('submit',function(e){
+	e.preventDefault();
+});
+
+//run function when the 'Say hello' button is clicked
+submitBtn.addEventListener('click',function(){
+	//store the entered name in web storage
+	localStorage.setItem('name',nameInput.value);
+
+	nameDisplayCheck();
+});
+
+//run function when the 'Forget' button is clicked
+forgetBtn.addEventListener('click',function(){
+	localStorage.removeItem('name');
+
+	nameDisplayCheck();
+});
+
+function nameDisplayCheck()
+{
+	//check whether the 'name' data item is stored in web Storage
+	if(localStorage.getItem('name'))
+	{
+		let name = localStorage.getItem('name');
+		h1.textContent = 'Welcome, ' + name;
+
+		personalGreeting.textContent = 'Welcome to our website, ' + name + '! We hope you have fun while you are here.'; 
+		//hide the 'remember' part of the form and show the 'forget' part
+		forgetDiv.style.display = 'block';
+		rememberDiv.style.display = 'none';
+	}
+	else
+	{
+		h1.textContent = 'Welcome to our website ';
+		personalGreeting.textContent = 'Welcome to our website. We hope you have fun while you are here.';
+		//hide the 'forget' part of the form and show the 'remember' part
+		forgetDiv.style.display = 'none';
+		rememberDiv.style.display = 'block';
+	}
+}
+
+document.body.onload = nameDisplayCheck();
