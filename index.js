e4f0a8a786cea6fc2eae70806a22ba9ea6d2841b
njc192 <32405757+njc192@users.noreply.github.com>
2019-12-19 19:08:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,114 @@
-// create needed constants
-const rememberDiv = document.querySelector('.remember');
-const forgetDiv = document.querySelector('.forget');
-const form = document.querySelector('form');
-const nameInput = document.querySelector('#entername');
-const submitBtn = document.querySelector('#submitname');
-const forgetBtn = document.querySelector('#forgetname');
 
-const h1 = document.querySelector('h1');
-const personalGreeting = document.querySelector('.personal-greeting');
+function autocomplete(inp) {
 
-form.addEventListener('submit',function(e){
-	e.preventDefault();
-});
+  myJson = getJson(inp);
+  arr = myJson["results"];
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
 
-//run function when the 'Say hello' button is clicked
-submitBtn.addEventListener('click',function(){
-	//store the entered name in web storage
-	localStorage.setItem('name',nameInput.value);
-
-	nameDisplayCheck();
-});
-
-//run function when the 'Forget' button is clicked
-forgetBtn.addEventListener('click',function(){
-	localStorage.removeItem('name');
-
-	nameDisplayCheck();
-});
-
-function nameDisplayCheck()
-{
-	//check whether the 'name' data item is stored in web Storage
-	if(localStorage.getItem('name'))
-	{
-		let name = localStorage.getItem('name');
-		h1.textContent = 'Welcome, ' + name;
-
-		personalGreeting.textContent = 'Welcome to our website, ' + name + '! We hope you have fun while you are here.'; 
-		//hide the 'remember' part of the form and show the 'forget' part
-		forgetDiv.style.display = 'block';
-		rememberDiv.style.display = 'none';
-	}
-	else
-	{
-		h1.textContent = 'Welcome to our website ';
-		personalGreeting.textContent = 'Welcome to our website. We hope you have fun while you are here.';
-		//hide the 'forget' part of the form and show the 'remember' part
-		forgetDiv.style.display = 'none';
-		rememberDiv.style.display = 'block';
-	}
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i]["display_title"].toString().substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i]["display_title"].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i]["display_title"] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
 }
 
-document.body.onload = nameDisplayCheck();
+async function getJson(inp)
+{
+	const url = "https://www.ifixit.com/api/2.0/suggest/" + inp + "?doctypes=device";
+	const response = await fetch(url);
+	const myJson = await response.json();
+	return myJson;
+
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
+autocomplete(document.getElementById("myInput"));
+
+
+ 
