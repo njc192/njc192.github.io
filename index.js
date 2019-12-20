486d32fe99f7286e5e8ece56b95b0bc9b5e7386e
njc192 <32405757+njc192@users.noreply.github.com>
2019-12-19 20:21:52 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-
+var arr = [];
 function autocomplete(inp) {
-
-  myJson = getJson(inp);
-  arr = myJson["results"];
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
   var currentFocus;
   /*execute a function when someone writes in the text field:*/
   inp.addEventListener("input", function(e) {
+    getJson(inp.value);
       var a, b, i, val = this.value;
       /*close any already open lists of autocompleted values*/
       closeAllLists();
@@ -20,19 +18,19 @@ function autocomplete(inp) {
       /*append the DIV element as a child of the autocomplete container:*/
       this.parentNode.appendChild(a);
       /*for each item in the array...*/
-
       for (i = 0; i < arr.length; i++) {
         /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i]["display_title"].toString().substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        console.log(arr[i]);
+        if (arr[i]["display_title"].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
           /*create a DIV element for each matching element:*/
           b = document.createElement("DIV");
           /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML = "<strong>" + arr[i]["display_title"].substr(0, val.length) + "</strong>";
           b.innerHTML += arr[i]["display_title"].substr(val.length);
           /*insert a input field that will hold the current array item's value:*/
           b.innerHTML += "<input type='hidden' value='" + arr[i]["display_title"] + "'>";
           /*execute a function when someone clicks on the item value (DIV element):*/
-              b.addEventListener("click", function(e) {
+          b.addEventListener("click", function(e) {
               /*insert the value for the autocomplete text field:*/
               inp.value = this.getElementsByTagName("input")[0].value;
               /*close the list of autocompleted values,
@@ -90,25 +88,23 @@ function autocomplete(inp) {
     var x = document.getElementsByClassName("autocomplete-items");
     for (var i = 0; i < x.length; i++) {
       if (elmnt != x[i] && elmnt != inp) {
-      x[i].parentNode.removeChild(x[i]);
+        x[i].parentNode.removeChild(x[i]);
+      }
     }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+
+  async function getJson(inp)
+  {
+      const url = "https://www.ifixit.com/api/2.0/suggest/" + inp + "?doctypes=device";
+      const response = await fetch(url);
+      const myJson = await response.json();
+      arr = myJson["results"];
   }
 }
 
-async function getJson(inp)
-{
-	const url = "https://www.ifixit.com/api/2.0/suggest/" + inp + "?doctypes=device";
-	const response = await fetch(url);
-	const myJson = await response.json();
-	return myJson;
-
-}
-/*execute a function when someone clicks in the document:*/
-document.addEventListener("click", function (e) {
-    closeAllLists(e.target);
-});
-}
+/*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
 autocomplete(document.getElementById("myInput"));
-
-
- 
